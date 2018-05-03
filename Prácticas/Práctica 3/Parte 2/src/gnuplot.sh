af9570f1d17a47662e7551abeb7cb5ac5d52d370
Carlos Sánchez Páez <karlossanpa@gmail.com>
2018-05-03 23:35:09 +0200

    Terminada P3.2

--- Prácticas/Práctica 3/Parte 2/src/gnuplot.sh
+++ Prácticas/Práctica 3/Parte 2/src/gnuplot.sh
@@ -12,8 +12,8 @@ plot 'vmc280.tsp' using 2:3 with lines
 set output 'a280_ime.png'
 plot 'ime280.tsp' using 2:3 with lines
 
-#set output 'a280_kruskal.png'
-#plot 'kruskal280.tsp' using 2:3 with lines
+set output 'a280_kruskal.png'
+plot 'kruskal280.tsp' using 2:3 with lines
 
 
 
@@ -27,8 +27,8 @@ plot 'vmc48.tsp' using 2:3 with lines
 set output 'att48_ime.png'
 plot 'ime48.tsp' using 2:3 with lines
 
-#set output 'att48_kruskal.png'
-#plot 'kruskal48.tsp' using 2:3 with lines
+set output 'att48_kruskal.png'
+plot 'kruskal48.tsp' using 2:3 with lines
 
 
 
@@ -41,8 +41,8 @@ plot 'vmc52.tsp' using 2:3 with lines
 set output 'berlin52_ime.png'
 plot 'ime52.tsp' using 2:3 with lines
 
-#set output 'berlin52_kruskal.png'
-#plot 'kruskal52.tsp' using 2:3 with lines
+set output 'berlin52_kruskal.png'
+plot 'kruskal52.tsp' using 2:3 with lines
 
 
 
@@ -55,8 +55,8 @@ plot 'vmc130.tsp' using 2:3 with lines
 set output 'ch130_ime.png'
 plot 'ime130.tsp' using 2:3 with lines
 
-#set output 'ch130_kruskal.png'
-#plot 'kruskal130.tsp' using 2:3 with lines
+set output 'ch130_kruskal.png'
+plot 'kruskal130.tsp' using 2:3 with lines
 
 
 
@@ -69,8 +69,8 @@ plot 'vmc96.tsp' using 2:3 with lines
 set output 'gr96_ime.png'
 plot 'ime96.tsp' using 2:3 with lines
 
-#set output 'gr96_kruskal.png'
-#plot 'kruskal96.tsp' using 2:3 with lines
+set output 'gr96_kruskal.png'
+plot 'kruskal96.tsp' using 2:3 with lines
 
 
 
@@ -84,8 +84,8 @@ plot 'vmc105.tsp' using 2:3 with lines
 set output 'lin105_ime.png'
 plot 'ime105.tsp' using 2:3 with lines
 
-#set output 'lin105_kruskal.png'
-#plot 'kruskal105.tsp' using 2:3 with lines
+set output 'lin105_kruskal.png'
+plot 'kruskal105.tsp' using 2:3 with lines
 
 
 
@@ -100,23 +100,23 @@ plot 'vmc561.tsp' using 2:3 with lines
 set output 'pa561_ime.png'
 plot 'ime561.tsp' using 2:3 with lines
 
-#set output 'pa561_kruskal.png'
-#plot 'kruskal561.tsp' using 2:3 with lines
+set output 'pa561_kruskal.png'
+plot 'kruskal561.tsp' using 2:3 with lines
 
 
 
 
-#set output 'st70_mejor.png'
-#plot 'mejor_70.tsp' using 2:3 with lines
+set output 'st70_mejor.png'
+plot 'mejor_70.tsp' using 2:3 with lines
 
-#set output 'st70_vmc.png'
-#plot 'vmc70.tsp' using 2:3 with lines
+set output 'st70_vmc.png'
+plot 'vmc70.tsp' using 2:3 with lines
 
-#set output 'st70_ime.png'
-#plot 'ime70.tsp' using 2:3 with lines
+set output 'st70_ime.png'
+plot 'ime70.tsp' using 2:3 with lines
 
-#set output 'st70_kruskal.png'
-#plot 'kruskal70.tsp' using 2:3 with lines
+set output 'st70_kruskal.png'
+plot 'kruskal70.tsp' using 2:3 with lines
 
 
 
@@ -129,8 +129,8 @@ plot 'vmc76.tsp' using 2:3 with lines
 set output 'pr76_ime.png'
 plot 'ime76.tsp' using 2:3 with lines
 
-#set output 'pr76_kruskal.png'
-#plot 'kruskal76.tsp' using 2:3 with lines
+set output 'pr76_kruskal.png'
+plot 'kruskal76.tsp' using 2:3 with lines
 
 
 
@@ -143,8 +143,8 @@ plot 'vmc100.tsp' using 2:3 with lines
 set output 'rd100_ime.png'
 plot 'ime100.tsp' using 2:3 with lines
 
-#set output 'rd100_kruskal.png'
-#plot 'kruskal100.tsp' using 2:3 with lines
+set output 'rd100_kruskal.png'
+plot 'kruskal100.tsp' using 2:3 with lines
 
 
 
@@ -157,8 +157,8 @@ plot 'vmc225.tsp' using 2:3 with lines
 set output 'tsp225_ime.png'
 plot 'ime225.tsp' using 2:3 with lines
 
-#set output 'tsp225_kruskal.png'
-#plot 'kruskal225.tsp' using 2:3 with lines
+set output 'tsp225_kruskal.png'
+plot 'kruskal225.tsp' using 2:3 with lines
 
 
 
@@ -172,5 +172,5 @@ plot 'vmc16.tsp' using 2:3 with lines
 set output 'ulysses16_ime.png'
 plot 'ime16.tsp' using 2:3 with lines
 
-#set output 'ulysses16_kruskal.png'
-#plot 'kruskal16.tsp' using 2:3 with lines
+set output 'ulysses16_kruskal.png'
+plot 'kruskal16.tsp' using 2:3 with lines
--- Prácticas/Práctica 3/Parte 2/src/tsp.cpp
+++ Prácticas/Práctica 3/Parte 2/src/tsp.cpp
@@ -5,6 +5,7 @@
 #include <cmath>
 #include <string>
 #include <limits>
+#include <list>
 using namespace std;
 
 #define TEST 0
@@ -13,6 +14,7 @@ struct ciudad {
 	int n;
 	double x;
 	double y;
+	//int grado = 0;
 };
 
 bool operator==(const ciudad &una, const ciudad &otra) {
@@ -159,6 +161,179 @@ private:
 		camino.insert(camino.begin() + indice, a_insertar);
 		visitados[a_insertar.n] = true;
 	}
+	/*
+		pair< pair<ciudad, ciudad>, double> calularMinimo(pair< pair<ciudad, ciudad>, double> &min,
+		        vector<vector <bool> > &matrizVisitados, vector<list<ciudad>> &conex, vector<bool> &aniadida) {
+			cout << "entra calcularminimo" << endl;
+
+			int x = 0;
+			int y = 0;
+			pair< pair<ciudad, ciudad>, double> resultado;
+			resultado.second = INFINITY;
+
+			for (int i = 0; i < matriz_distancias.size(); i++) {
+
+				for (int j = i + 1; j < matriz_distancias.size(); j++) {
+
+					if (ciudades[i].grado < 2 and ciudades[j].grado < 2) {
+
+						if (matriz_distancias[i][j] >= min.second and
+						        matriz_distancias[i][j] < resultado.second and
+						        matriz_distancias[i][j] != 0 and
+						        not matrizVisitados[i][j] and
+						        !hay_ciclo (conex, ciudades[i], ciudades[j], matrizVisitados)) {
+
+							resultado.second = matriz_distancias[i][j];
+							(resultado.first).first = ciudades[i];
+							(resultado.first).second = ciudades[j];
+							x = i;
+							y = j;
+						}
+					}
+				}
+			}
+
+
+			if (!conex[x].empty() and !aniadida[y] and not (conex[x].size() == ciudades.size() || conex[y].size() == ciudades.size())) {
+
+				if (conex[y].size() > 1) {
+
+					for (auto it = conex[y].begin(); it != conex[y].end(); ++it) {
+						cout << "pb1" << endl;
+
+						conex[x].push_back(*it);
+					}
+
+				}
+				else {
+					cout << "pb2" << endl;
+
+					conex[x].push_back(ciudades[y]);
+				}
+
+				aniadida[y] = true;
+				conex[y].clear();
+			}
+			else if (!conex[x].empty() and aniadida[y] and not (conex[x].size() == ciudades.size() || conex[y].size() == ciudades.size())) {
+
+				for (int k = 0; k < conex.size(); k++) {
+					for (auto it = conex[k].begin(); it != conex[k].end(); ++it) {
+
+						if (*it == ciudades[y]) {
+							if (conex[x].size() > 1) {
+
+								for (auto it2 = conex[x].begin(); it2 != conex[x].end(); ++it2) {
+									cout << "pb3" << endl;
+
+									conex[k].push_back(*it2);
+								}
+							}
+							else {
+								cout << "pb4" << endl;
+
+								conex[k].push_back(ciudades[x]);
+							}
+							aniadida[x] = true;
+							conex[x].clear();
+						}
+					}
+				}
+			}
+			else if (conex[x].empty() and !aniadida[y] and not (conex[x].size() == ciudades.size() || conex[y].size() == ciudades.size())) {
+
+				for (int k = 0; k < conex.size(); k++) {
+					for (auto it = conex[k].begin(); it != conex[k].end(); ++it) {
+
+						if (*it == ciudades[x]) {
+							if (conex[y].size() > 1) {
+
+								for (auto it2 = conex[y].begin(); it2 != conex[y].end(); ++it2) {
+									cout << "pb5" << endl;
+									conex[k].push_back(*it2);
+
+								}
+							}
+							else {
+								cout << "pb6" << endl;
+
+								conex[k].push_back(ciudades[y]);
+
+							}
+							aniadida[y] = true;
+							conex[y].clear();
+						}
+					}
+				}
+			}
+
+			matrizVisitados[x][y] = true;
+			ciudades[x].grado += 1;
+			ciudades[y].grado += 1;
+
+			cout << "Fin calculaMin" << endl;
+
+			return resultado;
+		}
+
+		bool hay_ciclo(vector<list<ciudad>> &conex, const ciudad & primera, const ciudad & segunda, vector<vector <bool> > &matrizVisitados) {
+
+			cout << "entra hay ciclo" << endl;
+			bool primera_enc = false;
+			bool hay = false;
+			ciudad restante;
+			bool primera_vez = true;
+
+			for (int i = 0; i < conex.size(); i++) {
+				for (auto it = conex[i].begin(); it != conex[i].end() && !hay; ++it) {
+
+					if (*it == primera || *it == segunda && primera_vez) {
+
+						primera_enc = true;
+						primera_vez = false;
+
+						if (*it == primera) {
+
+							restante = segunda;
+						}
+						else {
+
+							restante = primera;
+						}
+					}
+
+					if (primera_enc && *it == restante) {
+
+						hay = true;
+						matrizVisitados[primera.n][segunda.n] = true;
+					}
+				}
+
+				primera_vez = true;
+			}
+			cout << "Fin hayCiclo" << endl;
+			return (hay);
+		}
+	*/
+
+	pair<int, int> ArcoMenor() {
+		pair<int, int> resultado;
+		double d_minima = numeric_limits<double>::max();
+		for (int i = 0; i < ciudades.size(); i++) {
+			for (int j = 0; j < ciudades.size(); j++) {
+				if (i != j && !visitados[i] && !visitados[j]) {
+					if (matriz_distancias[i][j] < d_minima) {
+						d_minima = matriz_distancias[i][j];
+						resultado.first = i;
+						resultado.second = j;
+					}
+				}
+			}
+		}
+
+		visitados[resultado.first] = true;
+		visitados[resultado.second] = true;
+		return resultado;
+	}
 
 public:
 	TSP() {
@@ -173,6 +348,24 @@ public:
 	~TSP() {
 
 	}
+
+	void DerivadoKruskal() {
+		camino.clear();
+		while (camino.size() != ciudades.size()) {
+			pair<int, int> arcoMenor = ArcoMenor();
+			camino.push_back(ciudades[arcoMenor.first]);
+			camino.push_back(ciudades[arcoMenor.second]);
+		}
+		for (int i = 0; i < ciudades.size(); i++)
+			if (!visitados[i]){
+				camino.push_back(ciudades[i]);		//Si queda alguna por visitar. Pasa cuando NCIUDADES es impar
+				visitados[i]=true;
+			}
+		camino.push_back(camino[0]);
+		distancia_total = calcularDistanciaCamino(camino);
+
+	}
+
 
 	void VecinoMasCercano() {
 		pair<vector<ciudad>, double> minimo, temp;
@@ -196,9 +389,6 @@ public:
 		return ciudades.size();
 	}
 
-	void DerivadoKruskal() {
-
-	}
 
 	void CargarDatos(char *archivo) {
 		ifstream datos;
@@ -274,7 +464,65 @@ public:
 		distancia_total = calcularDistanciaCamino(camino);
 
 	}
+	/*
+		void algoritmo_de_laura_y_jose() {
+			vector< pair<ciudad, ciudad> > aristas;
+			pair< pair<ciudad, ciudad>, double> arista_minima;
+			ciudad ciudad_actual;
+			vector<vector <bool> > matrizVisitados (matriz_distancias.size(), vector<bool>(matriz_distancias.size(), false));
+			vector<list<ciudad>> ciudades_conex;
+			vector<bool> aniadida (ciudades.size(), false);
 
+			ciudades_conex.resize (ciudades.size());
+
+			//Cada ciudad está conexa consigo mismo
+			for (int in = 0; in < ciudades_conex.size(); in++)
+				ciudades_conex[in].push_back (ciudades[in]);
+
+			arista_minima.second = 0;
+
+			for (int i = 0; i < ciudades.size(); i++) {
+
+				arista_minima = calularMinimo(arista_minima, matrizVisitados, ciudades_conex, aniadida);
+				cout << "i= " << i << endl;
+				aristas.push_back(arista_minima.first);
+				distancia_total += arista_minima.second;
+			}
+
+			vector<bool> arista_leida (aristas.size(), false);
+
+			camino.push_back(aristas[0].first);
+			camino.push_back(aristas[0].second);
+			ciudad_actual = aristas[0].second;
+			arista_leida[0] = true;
+			for (auto it = matrizVisitados.begin(); it != matrizVisitados.end(); ++it)
+				it->clear();
+			matrizVisitados.clear();
+
+			for (auto it = aristas.begin(); it != aristas.end(); ++it)
+				cout << "(" << it->first.n << "," << it->second.n << ") ";
+			cout << endl;
+
+			while (camino.size() < ciudades.size()) {
+
+				for (int i = 1; i < aristas.size(); i++) {
+
+					if (ciudad_actual == aristas[i].first && !arista_leida[i]) {
+
+						ciudad_actual = aristas[i].second;
+						camino.push_back(ciudad_actual);
+						arista_leida[i] = true;
+					}
+					else if (ciudad_actual == aristas[i].second && !arista_leida[i]) {
+
+						ciudad_actual = aristas[i].first;
+						camino.push_back(ciudad_actual);
+						arista_leida[i] = true;
+					}
+				}
+			}
+		}
+	*/
 };
 
 
@@ -334,6 +582,7 @@ int main(int argc, char **argv) {
 
 	cout << "Heurística derivada de Kruskal." << endl;
 	derivado_kruskal.DerivadoKruskal();
+	//derivado_kruskal.algoritmo_de_laura_y_jose();
 	derivado_kruskal.imprimirResultado();
 
 	nombre = "kruskal";
